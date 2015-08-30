09b123b6af46aca08e6a64508abf100b218eadb6
Kyle Chickering <kylechickering@yahoo.com>
2015-08-30 00:21:04 -0600

    update ACCESS_TOKEN

--- ACCESS_TOKEN.js
+++ ACCESS_TOKEN.js
@@ -1,9 +1,6 @@
 /*
- Handle browser based authentication tokens on a user basis to secure account information and
- maintain API OAuth authentication standards
-
  for testing and ease of use you can use a personal access token here instead of going through the authentication
- do not push your access token to a public repository or share this token with anyone, treat it like a password.
+ do not push your access token or this file to a public repository or share this token with anyone, treat it like a password.
  */
 
-var ACCESS_TOKEN = "";
+var ACCESS_TOKEN = "insert personal access token here";

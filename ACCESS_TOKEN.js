79209f677e3819f27330a125bf356f300bb323ee
Kyle Chickering <kylechickering@yahoo.com>
2015-08-29 23:57:08 -0600

    added README, .gitignore, and ACCESS_TOKEN

---
+++ ACCESS_TOKEN.js
@@ -0,0 +1,9 @@
+/*
+ Handle browser based authentication tokens on a user basis to secure account information and
+ maintain API OAuth authentication standards
+
+ for testing and ease of use you can use a personal access token here instead of going through the authentication
+ do not push your access token to a public repository or share this token with anyone, treat it like a password.
+ */
+
+var ACCESS_TOKEN = "";

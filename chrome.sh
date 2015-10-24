117f3ef8ed58b621adf77d904242db346afd6261
KyroChi <kylechickering@yahoo.com>
2015-10-24 14:59:00 -0600

    makefile -> shell

--- chrome.sh
+++ chrome.sh
@@ -1,3 +1,12 @@
 #!/BIN/BASH
 
-make build_chrome
+rm -rf chrome_development/
+mkdir chrome_development
+cp src/js/content.js chrome_development/
+cp src/json/github_colors.js chrome_development/
+cp src/icon/icon-16.png chrome_development/
+cp src/icon/icon-32.png chrome_development/
+cp src/icon/icon-64.png chrome_development/
+cp src/icon/icon-128.png chrome_development/
+cp chrome/manifest.json chrome_development/
+echo chrome development directory created
--- firefox.sh
+++ firefox.sh
@@ -1,6 +1,19 @@
 #!/BIN/BASH
 
-make build_firefox
+rm -rf firefox_development/
+mkdir firefox_development/
+cp firefox/package.json firefox_development/
+mkdir firefox_development/data/
+cp firefox/firefox.js firefox_development/data/
+cp src/js/content.js firefox_development/data/
+cp src/icon/icon-16.png firefox_development/data/
+cp src/icon/icon-32.png firefox_development/data/
+cp src/icon/icon-64.png firefox_development/data/
+cp src/js/content.js firefox_development/data/
+cp src/json/github_colors.js firefox_development/data/
+
+echo firefox development directory created
+
 cd firefox_development/
 jpm run
 cd ..
